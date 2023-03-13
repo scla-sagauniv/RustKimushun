471bc0f3b221a38278caa0b42b37181ca5171f24
nnnnn81 <nnnnnagi81@gmail.com>
2023-03-14 01:27:02 +0900

    update: シャッターボタンの追加

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,5 @@
-pub mod button;
 pub mod back_button;
+pub mod button;
 pub mod camera;
 pub mod home;
+pub mod shutter_button;
--- src/components/camera.rs
+++ src/components/camera.rs
@@ -1,4 +1,4 @@
-use crate::components::back_button::BackButton;
+use crate::components::{back_button::BackButton, shutter_button::ShutterButton};
 use yew::{function_component, html, Html};
 
 #[function_component(Camera)]
@@ -6,10 +6,15 @@ pub fn camera() -> Html {
     html! {
       <>
         <div class="container">
-          <div class="text-center mt-4">
-            <h3>{"作成"}</h3>
-          </div>
           <BackButton title={""} destination={"/"}/>
+          <div  class="d-flex flex-column align-items-center">
+            <div class="position-absolute top-0 mt-4">
+              <h3>{"作成"}</h3>
+            </div>
+            <div class="position-absolute bottom-0 mb-5">
+              <ShutterButton title={""} destination={"/"}/>
+            </div>
+          </div>
         </div>
       </>
     }
