21ec5353b465c9f7290c3f03ff6222ae1328c7f7
nnnnn81 <nnnnnagi81@gmail.com>
2023-03-15 03:42:40 +0900

    #19 update: ocrの認識制御した

--- src/logic/encode.js
+++ src/logic/encode.js
@@ -33,6 +33,9 @@ async function ocr(imgPath) {
     await worker.load();
     await worker.loadLanguage("eng");
     await worker.initialize("eng");
+    await worker.setParameters({
+      tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
+    });
     const {
       data: { text, symbols },
     } = await worker.recognize(
